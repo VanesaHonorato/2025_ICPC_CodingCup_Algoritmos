34f1c45f781996855a3b55dbb80d8df70a0e778f
Vanesa Honorato <vanegoho@gmail.com>
2025-01-22 20:55:07 -0600

    ICPC intialrepository local

---
+++ c++/Test.cpp
@@ -0,0 +1,20 @@
+//ITESS TICS 
+//ICPC VGH
+//COMPILACION JAVA Y EJECUCION 
+//20-ENERO-25
+//vanegoho@gmail.com
+
+/*c++  Test.cpp -o Test.exe
+ *Test.exe
+ */
+
+#include<iostream>
+
+using  namespace std;
+
+
+int main()
+{
+    std::cout <<"hello, programmers"<< std::endl;
+
+}
---
+++ java/Test.java
@@ -0,0 +1,16 @@
+//ITESS TICS 
+//ICPC VGH
+//COMPILACION JAVA Y EJECUCION 
+//20-ENERO-25
+//vanegoho@gmail.com
+
+/*javac  Test.java
+ * java Test
+ */
+
+
+public class Test{
+    public static void main(String[] arg){
+        System.out.println("Hello, programmers");
+    }
+}
